c5b6b03792b2941aaa2a980fc977d25c82e29fe7
Abdullahil Ashik Md. Arefin <abdullah.ashik.arefin@gmail.com>
2022-10-14 22:58:11 +0600

    djrest

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
-from .models import UserProfile
 
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('name','email','is_staff','is_superuser','is_active')
-
-admin.site.register(UserProfile, UserProfileAdmin)
+# Register your models here.
--- profiles/models.py
+++ profiles/models.py
@@ -1,60 +1,45 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
-from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import AbstractUser, PermissionsMixin, BaseUserManager
+
 
 class UserProfileManager(BaseUserManager):
-    ''' custom user object manager for the user in system '''
+    ''' custome manager for user profile model '''
 
     def create_user(self, email, name, password=None):
-        ''' create user in the system '''
+        ''' create a new user profile '''
         if not email:
-            raise ValueError('You need to provide a valid email for the user')
-        
-        email = self.normalize_email(email)
-        user = self.model(name=name, email=email)
+            raise ValueError('Email is required')
+
+        clean_email = self.normalize_email(email)
+        user = self.model(email=clean_email, name=name)
         user.set_password(password)
         user.save(using=self._db)
-
         return user
-    
+
     def create_superuser(self, email, name, password):
-        ''' create super user in the system '''
+        pass
 
-        user = self.create_user(email,name,password)
 
-        
-        user.is_superuser = True # this particular column is giving me errors
-        user.is_staff = True
-        
-
-        user.save(using=self._db)
-        return user
-
-class UserProfile(AbstractBaseUser, PermissionsMixin):
-    ''' database model for user in system '''
+class UserProfile(AbstractUser, PermissionsMixin):
+    ''' Database model for the custom user table '''
+    email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
-    email = models.EmailField(max_length=50, unique=True)
-    
-    is_staff = models.BooleanField(default=True)
     is_active = models.BooleanField(default=True)
-    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name']
 
-    objects = UserProfileManager()
+    def get_full_name(self):
+        ''' get user's full name '''
+        return self.name
 
     def get_short_name(self):
-        ''' return short name for the user '''
+        ''' user short name function / method '''
         return self.name
-    
-    def get_full_name(self):
-        ''' return full name for the user '''
-        return self.name
-    
+
     def __str__(self):
-        return self.name
-    
-    class Meta:
-        verbose_name = 'User'
-        verbose_name_plural = 'All Users'
+        ''' string representation of object '''
+        return self.email
